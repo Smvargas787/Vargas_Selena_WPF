d5db8ca00c9b2d4549f6d1bd5d7d05012acf33a4
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-10-31 17:27:25 -0400

    Slice Pie Part 2 (9)
    C&S 9

--- Day 3/Day3LabExpressions/js/script.js
+++ Day 3/Day3LabExpressions/js/script.js
@@ -8,7 +8,7 @@
 //Slice of Pie Part 1
 // Calculate how many slices each person will get at the party
 
-//Given:
+//Given
 var slices = 2;
 var people = 20;
 var ordered = 5;
@@ -23,3 +23,11 @@ console.log(pizzaSolution);
 //Result Format
 var together = "Each person ate "+pizzaSolution+" slices of pizza at the party.";
 console.log(together);
+
+//Slice of Pie Part 2
+//Calculate how many leftover slices there will be for sparky
+
+//Given
+var slices = 2;
+var people = 20;
+var ordered = 5;
