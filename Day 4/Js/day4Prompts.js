1fe902384dd45833efed9e79ed43da7cd3b5cb20
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-03 18:51:09 -0500

    Calculate Age based on year
    Learning about Prompts

---
+++ Day 4/Js/day4Prompts.js
@@ -0,0 +1,18 @@
+/*
+Selena Vargas
+Day 4 Prompts
+WPF 1411
+11-3-2014
+ */
+
+alert("Testing 1, 2, 3!");
+
+//Ask user for Input
+
+//Prompt() - ask the user to type something in
+var yearBorn = prompt("Enter your year of birth:");
+console.log(yearBorn);
+
+//Figure out how old we are
+var age = 2014 - yearBorn;
+console.log("You are "+age+" years old.");
