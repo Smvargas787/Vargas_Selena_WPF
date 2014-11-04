cfc3c3c372e35e330a65612a886367a4b2457809
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-04 11:02:15 -0500

    Wacky (7)
    C&S 7

---
+++ Expressions_Assignment/Expressions_Wacky/Js/wackyExpress.js
@@ -0,0 +1,19 @@
+/*
+Selena Vargas
+Wacky Expressions
+WPF 1411
+11-3-14
+ */
+
+alert("Ha Ha! You have fallen into my comedy trap!");
+
+var jokeIntro = prompt("The following are going to be a series of knock knock jokes that you must laugh obnoxiously to\n" +
+"or walk away with the thought in your mind that you just crushed my comedian career. Click ok to continue.");
+console.log("The following are going to be a series of knock knock jokes that you must laugh obnoxiously to\n" +
+"or walk away with the thought in your mind that you just crushed my comedian career. Click ok to continue. "+jokeIntro+"");
+
+//Joke 1
+var joke1 = prompt("Round 1!\nKnock knock!\n(1): Who's There?");
+console.log("Round"+joke1+" 1!\nKnock knock!\n1:Who's There?");
+
+var
