1cce617fe13c6f768971605616dd85b62a3d059d
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-04 11:28:50 -0500

    Wacky (8)
    C&S 8

--- Expressions_Assignment/Expressions_Wacky/Js/wackyExpress.js
+++ Expressions_Assignment/Expressions_Wacky/Js/wackyExpress.js
@@ -16,4 +16,16 @@ console.log("The following are going to be a series of knock knock jokes that yo
 var joke1 = prompt("Round 1!\nKnock knock!\n(1): Who's There?");
 console.log("Round"+joke1+" 1!\nKnock knock!\n1:Who's There?");
 
-var
+var actualJoke = prompt("Boo!\n(2): Boo who?");
+console.log("Boo!\n("+actualJoke+"):Boo who?");
+
+
+var jokeAnswer = prompt("Awwh, don't cry. I'm just telling you jokes in alert forms.\n(1):Kevin Hart Material");
+console.log("Awwh, don't cry. I'm just telling you jokes in alert forms.\n("+jokeAnswer+"):Kevin Hart Materal.");
+
+var hilarous = "Kevin Hart";
+console.log("I know, I know, I'm the next "+hilarous+".");
+
+
+//Joke 2
+var joke2 =
