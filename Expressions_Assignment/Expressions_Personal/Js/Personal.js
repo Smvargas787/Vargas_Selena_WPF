6453ed87d6a2249dfd7c55d2380d9095f79c33d6
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-03 23:01:43 -0500

    Day 4 Personal Expressions (2)
    C&S 2

--- Expressions_Assignment/Expressions_Personal/Js/Personal.js
+++ Expressions_Assignment/Expressions_Personal/Js/Personal.js
@@ -5,10 +5,16 @@ WPF 1411
 11-3-14
  */
 
-alert("Underground Station has a membership discount that allows you to get $10 off of every $25 purchase" +
+alert("Footlocker has a membership discount that allows you to get $10 off of every purchase" +
 "\nAfter buying x amount of shoes how much does your discount total up to?");
 
 var numShoes = prompt("How many shoes do you plan on buying?");
  console.log("You bought "+numShoes+" pairs of shoes.");
+
+
+ var shoeCost = prompt("How much did each shoe cost?");
+ console.log("Each pair purchased was $"+shoeCost+".");
+
+
 
 
