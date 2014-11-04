6ae54e5086f66cbab94e6f97521e2e746da90d9b
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-03 19:08:24 -0500

    Finished Prompting Exercies
    Console area

--- Day 4/Js/day4Prompts.js
+++ Day 4/Js/day4Prompts.js
@@ -16,3 +16,28 @@ console.log(yearBorn);
 //Figure out how old we are
 var age = 2014 - yearBorn;
 console.log("You are "+age+" years old.");
+
+
+//Calculate the area of a rectangle
+//area = length * width
+
+var length = prompt("We are going to calculate the area of a rectangle.\nPlease enter the length of your rectangle:");
+
+var width = prompt("Please enter the width of your rectangle:");
+
+//Calculate the actual area
+var area = length * width;
+
+//Create a results variable and then console.log that
+var results = "The area of your rectangle with a width of "+width+" and a length of "+length+" is "+area+".";
+
+console.log(results);
+alert(results);
+
+//Console the area
+console.log("The area of your rectangle with a width of "+width+" and a length of "+length+" is "+area+".");
+
+//Alert the user with the same information
+alert("The area of your rectangle with a width of "+width+" and a length of "+length+" is "+area+".");
+
+
