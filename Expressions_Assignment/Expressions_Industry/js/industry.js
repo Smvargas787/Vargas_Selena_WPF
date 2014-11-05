79e32f7dcc68f2aba077ef63c674d663b34ef9ae
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-04 22:27:03 -0500

    Industry Express (11)
    C&S 11

---
+++ Expressions_Assignment/Expressions_Industry/js/industry.js
@@ -0,0 +1,12 @@
+ /*
+Selena Vargas
+Industry Expressions
+WPF 1411
+11/4/14
+ */
+
+alert("Your father is making a sprinkler business and wants you to design a website for his new found company Water Works.\nAssuming his company logo has yet to be made, we want to figure out how much the perimeter is to cover the whole header.");
+
+//Calculate Perimeter
+ //
+
