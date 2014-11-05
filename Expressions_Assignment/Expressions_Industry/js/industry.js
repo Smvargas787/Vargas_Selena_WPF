882d836da7dc22835d0012fccf2c71bc55454707
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-04 22:38:25 -0500

    Industry (12)
    C&S 12

--- Expressions_Assignment/Expressions_Industry/js/industry.js
+++ Expressions_Assignment/Expressions_Industry/js/industry.js
@@ -5,8 +5,19 @@ WPF 1411
 11/4/14
  */
 
-alert("Your father is making a sprinkler business and wants you to design a website for his new found company Water Works.\nAssuming his company logo has yet to be made, we want to figure out how much the perimeter is to cover the whole header.");
+alert("Your father is making a sprinkler business and wants you to design a website for his new found company Water Works.\nAssuming his company logo has yet to be made, we want to figure out how much the perimeter is of a square shaped logo to cover the whole header.");
 
-//Calculate Perimeter
- //
+//Calculate square perimeter
+ //perimeter = 4a
 
+var a = prompt("Lets set up our calculations, please enter the length and width that you prefer for the square logo?");
+ var 4;= prompt("To find the perimeter of a square we must multiply the length and width by 4, please enter the number 4 now.");
+
+//Correct Calculations
+
+ var perimeter = a * 4;
+
+ var answer = "The perimeter of Water Works square logo is now "+perimeter+"in.";
+ console.log(answer);
+ alert(answer);
+ 
