ddeef99fada3ddf78ddaf141a1178941a2b9eab5
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-04 23:03:58 -0500

    Industry (13)
    C&S 13

--- Expressions_Assignment/Expressions_Industry/js/industry.js
+++ Expressions_Assignment/Expressions_Industry/js/industry.js
@@ -5,13 +5,13 @@ WPF 1411
 11/4/14
  */
 
-alert("Your father is making a sprinkler business and wants you to design a website for his new found company Water Works.\nAssuming his company logo has yet to be made, we want to figure out how much the perimeter is of a square shaped logo to cover the whole header.");
+alert("Bob the Builder is making a sprinkler business and wants you to design a website for his new found company Water Works.\nAssuming his company logo has yet to be made, we want to figure out how much the perimeter is of a square shaped logo to cover the whole header.");
 
 //Calculate square perimeter
  //perimeter = 4a
 
 var a = prompt("Lets set up our calculations, please enter the length and width that you prefer for the square logo?");
- var 4;= prompt("To find the perimeter of a square we must multiply the length and width by 4, please enter the number 4 now.");
+ var numFour; prompt("To find the perimeter of a square we must multiply the length and width by 4, please enter the number 4 now.");
 
 //Correct Calculations
 
@@ -20,4 +20,15 @@ var a = prompt("Lets set up our calculations, please enter the length and width 
  var answer = "The perimeter of Water Works square logo is now "+perimeter+"in.";
  console.log(answer);
  alert(answer);
- 
+
+alert("Now that we've placed the logo perfectly on the header of the website, Bob wants us to make sure we add in his 3 different locations he runs his business from.");
+
+ var firstLocation = prompt("Please enter the first location you wish for Bob to serve in.") ;
+ var secondLocation = prompt("Second location you wish for Bob to serve in.");
+ var thirdLocation = prompt("Third location you wish for Bob to serve in.");
+
+
+ var bobsLocations = firstLocation+","+secondLocation+",& " +thirdLocation+".";
+ console.log(bobsLocations);
+
+
