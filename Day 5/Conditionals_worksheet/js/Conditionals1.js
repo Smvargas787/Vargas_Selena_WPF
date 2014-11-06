d8810f6c2c64d2af355a32f00e27ca7b971dbb9e
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-05 23:16:32 -0500

    Gas Tank (2)
    Added math

--- Day 5/Conditionals_worksheet/js/Conditionals1.js
+++ Day 5/Conditionals_worksheet/js/Conditionals1.js
@@ -9,16 +9,22 @@ WPF 1411
 
 //Last Chance for Gas
 
- var gasEfficiency =17;
- var gaugeTank = 20;
- var tankCapacity = 12;
+ var gasEfficiency =prompt("Please tell me your fuel efficiency?");
+ var gaugeTank = prompt("What percent of fuel do you have?");
+ var tankCapacity = prompt("How many gallons do you have?");
 
- if(gaugeTank>60){
+
+
+ var mpg = (gaugeTank/100)*tankCapacity*gasEfficiency;
+
+//If statement
+ if(mpg>=200){
   console.log("Yes, you can make it without stopping for gas! Now go faster!");
+
 
  } else {
   console.log("You better go and get some gas before you get stuck forever and ever!\n" +
-  "You only have "+tankCapacity+" gallons left.")
+  "You can only go as far "+mpg+" miles before you need more gas.");
 
 
  }
