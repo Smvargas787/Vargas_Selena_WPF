d2409f4b7b25a39c75b9a7348e7f3a1e20a64d22
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-05 23:46:57 -0500

    GPA Calculator (3)
    Making more prompts !

---
+++ Day 5/Conditionals_worksheet/js/GPA.js
@@ -0,0 +1,43 @@
+ /*
+Selena Vargas
+GPA Conditional
+WPF 1411
+11/5/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+
+//Grade Letter Calculator
+
+ var gpa = prompt("What is your current grade here at Full Sail?");
+
+ if(gpa=100){
+  console.log("A++! We have an overachiever in the building!")
+
+ }
+
+ if(gpa>=95){
+  console.log("A+! Way to go you wise kid you!");
+
+ } else if(gpa>=85){
+  console.log("B+! You're moving on up! Keep it up!")
+
+ } else if(gpa>=80){
+  console.log("You got a B without getting stung! Good job!");
+
+
+ } else if(gpa>=75){
+  console.log("C+ is always still passing for me!");
+
+ } else if(gpa>=70) {
+  console.log("No more of that mini heart attack, You've managed to pass your course with a C!");
+
+ } else if(gpa<=65){
+  console.log("I would rethink your choices in life at this point. You've managed to fail your course with a D.");
+
+ } else if(steakTemp<=160){
+  console.log("Your steak is Well Done");
+
+ } else{
+  console.log("Your burn your steak. Hope you like it crispy");
