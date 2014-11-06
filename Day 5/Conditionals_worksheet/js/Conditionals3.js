c69a314f24d844b19f1fb625cca7eb3faf095c94
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-06 00:58:15 -0500

    Finished Group Conditionals

--- Day 5/Conditionals_worksheet/js/Conditionals3.js
+++ Day 5/Conditionals_worksheet/js/Conditionals3.js
@@ -5,12 +5,22 @@ WPF 1411
 11/5/14
  */
 
-alert("Testing 1, 2, 3!");
+//alert("Testing 1, 2, 3!");
 
 
 //Movie Ticket Given
 
-var age = prompt("How old are you?");
 var movieTime = prompt("What is the time of your movie?");
+var age = prompt("How old are you?");
 
+//If statement
 
+ if ((movieTime>=3 && movieTime<=5) || (age<=10 || age>=55 )) {
+  console.log("Your ticket is now $7.00, enjoy your movie with your awesome discount!");
+
+ }
+
+ else {
+   console.log("Your ticket price is $12.00. That sucks.")
+
+}
