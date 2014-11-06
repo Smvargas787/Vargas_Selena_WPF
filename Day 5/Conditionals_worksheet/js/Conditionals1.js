f807869c39a0d4b5a40eabcf8604d8e23fd08ddd
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-05 22:39:08 -0500

    Gas Tank Conditional
    C&S 1

---
+++ Day 5/Conditionals_worksheet/js/Conditionals1.js
@@ -0,0 +1,24 @@
+ /*
+Selena Vargas
+Conditionals Worksheet
+WPF 1411
+11/5/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+//Last Chance for Gas
+
+ var gasEfficiency =17;
+ var gaugeTank = 20;
+ var tankCapacity = 12;
+
+ if(gaugeTank>60){
+  console.log("Yes, you can make it without stopping for gas! Now go faster!");
+
+ } else {
+  console.log("You better go and get some gas before you get stuck forever and ever!\n" +
+  "You only have "+tankCapacity+" gallons left.")
+
+
+ }
--- Day 5/js/day5Ternary.js
+++ Day 5/js/day5Ternary.js
@@ -5,8 +5,51 @@ WPF 1411
 11/5/14
  */
 
-alert("Testing 1, 2, 3!");
+//alert("Testing 1, 2, 3!");
 
 
+//If your gpa is greater than 2.0 then you graduate
 
+
+var gpa = 3.5;
+
+
+if(gpa>2.0){
+ console.log("Congrats you graduate!");
+
+
+} else {
+ console.log("Sorry, study more!");
+
+
+}
+
+//Convert to a ternary
+// (condition to test) ? true code:else code;
+
+(gpa>2.0) ? console.log("Congrats you graduate!") : console.log("Sorry, study more!");
+
+//Set a variable equal to the outcome
+//Kids under 10 read Green Eggs and Ham
+//Kids 10 and older read The Time Machine
+
+var age = 16;
+var book;
+
+book = (age>=10) ? "The Time Machine": "Green Eggs and Ham";
+
+//Kids 14 and older read Twilight
+book = (age<10) ? "Green Eggs and Ham" : (age<14) ? "The Time Machine" : "Twilight";
+
+console.log(book);
+
+
+//Longer/Regular way to code it
+if(age>=10){
+ book = "The Time Machine"
+
+} else{
+ book ="Green eggs and ham"
+
+}
 
