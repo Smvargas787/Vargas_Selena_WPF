60b6ae41ee327532741bbccd89166438bd488fa2
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-08 01:20:55 -0500

    Industry (14)

--- Day 6/Conditionals_Industry/js/indsutry.js
+++ Day 6/Conditionals_Industry/js/indsutry.js
@@ -11,4 +11,7 @@ alert("Testing 1, 2, 3!");
  //Website Pricing
 
 alert("It's time to make Selena money!");
- alert("WWE is wanting to make new hires for their web designing field and showed you ")
+ alert("A local Web Designing company is wanting to make new hires for their web development field and showed you a list of their hourly salaries " +
+ "they are willing to pay you.");
+
+ 
