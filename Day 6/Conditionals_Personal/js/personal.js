7e7993387de4e62458eadac29f09210f7ae00301
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-07 23:40:28 -0500

    Personal (1)

---
+++ Day 6/Conditionals_Personal/js/personal.js
@@ -0,0 +1,22 @@
+/*
+ Selena Vargas
+Personal Conditionals
+WPF 1411
+11/7/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+//Phone Tragedy Scenario
+
+alert("Your phone recently just broke and your bank account is completely empty from the plane tickets you just bought." +
+" However you need a new phone before you leave in 6 weeks.");
+
+var job = prompt("How much do you make an hour at your job?");
+var  hours = prompt("How many hours do you work a week?");
+var  government = prompt("How much does the government take out of your paycheck?");
+var tripMoney = 700;
+
+var paycheck = (job*hours)-government;
+
+
