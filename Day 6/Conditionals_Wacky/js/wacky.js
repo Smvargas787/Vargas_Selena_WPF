d55119716384842ff83a23bfb80ebc6eb7b65680
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-08 00:54:55 -0500

    Wacky (11)

--- Day 6/Conditionals_Wacky/js/wacky.js
+++ Day 6/Conditionals_Wacky/js/wacky.js
@@ -12,9 +12,9 @@ WPF 1411
 //Spongebob Scenario
 
  alert("Spongebob Time!");
- alert("Mr. Krabs wants to take his daughter Pearl out on a crab and whale date. The tickets are originally $9:00 " +
+ alert("Mr. Krabs wants to take his daughter Pearl out on a crab and whale date. The tickets are originally $9.00 " +
  "but whales get a discount if they're a certain height because they can barely fit in the theater. Considering that Mr. Krabs " +
- "is cheap as well, he notices that if he goes during the 10:00am-12:00pm showing the discount is $1:00 instead.");
+ "is cheap as well, he notices that if he goes during the 10:00am-12:00pm showing the discount is $1.00 instead.");
 
 
 var discountHeight = 66.6;
