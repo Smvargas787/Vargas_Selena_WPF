d5b9623a7194e913a375421f4ed2e761485827b3
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-08 00:17:48 -0500

    Wacky (6)

---
+++ Day 6/Conditionals_Wacky/js/wacky.js
@@ -0,0 +1,23 @@
+
+ /*
+Selena Vargas
+Wacky Conditionals
+WPF 1411
+11/7/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+
+//Spongebob Scenario
+
+ alert("Spongebob Time!");
+ alert("Mr. Krabs wants to take his daughter Pearl out on a crab and whale date. The tickets are originally $9:00" +
+ "but whales get a discount of $5:00 if they're a certain height because they can barely fit in the theater.");
+
+
+var discountHeight = 66.6;
+ var pearlsHeight = prompt("What is Pearl's current height?");
+
+ 
+
