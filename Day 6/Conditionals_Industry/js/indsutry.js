353e612947f76fbdf737d165b8f6ab6b37971b0b
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-08 01:18:23 -0500

    Industry (13)

---
+++ Day 6/Conditionals_Industry/js/indsutry.js
@@ -0,0 +1,14 @@
+
+ /*
+Selena Vargas
+Conditionals Industry
+WPF 1411
+11/7/14
+ */
+
+alert("Testing 1, 2, 3!");
+
+ //Website Pricing
+
+alert("It's time to make Selena money!");
+ alert("WWE is wanting to make new hires for their web designing field and showed you ")
