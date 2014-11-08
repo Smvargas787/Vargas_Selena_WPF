5fab58d92cd493fe7b8747ea9de51cb7e43265c8
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-07 19:23:26 -0500

    Learning about rounding
    Math.round

--- Day 6/js/day6Math.js
+++ Day 6/js/day6Math.js
@@ -5,6 +5,62 @@ WPF 1411
 11/7/14
  */
 
-alert("Testing 1, 2, 3!");
+//alert("Testing 1, 2, 3!");
 
 
+//Normal Rounding
+ //.5+ goes up, else goes down
+
+ var num1 = 9.544;
+ console.log(num1);
+ console.log(Math.round(num1));
+
+ //Floor Rounding
+ //Always rounds down
+
+var num2 = 6.8;
+ console.log(num2);
+ console.log(Math.floor(num2));
+
+ //Ceiling round
+ //Always rounds up
+
+ var num3 = 4.8;
+ console.log(num3);
+ console.log(Math.ceil(num3));
+
+ //How to get specific number of decimals
+ //toFixed - not part of Math
+
+ var num4 = 6.7893;
+ console.log(num4);
+ console.log(num4.toFixed(2));
+
+ //Random Numbers
+ //Math.random() - gives values 0 and almost 1.
+ //Nothing goes inside of the ()
+
+ var num5 = Math.random();
+ console.log(num5);
+
+ //Number 0-10
+ var num6 = Math.random()*10;
+ console.log(num6);
+
+ //Integers between 0-10
+ var num7 = Math.round( Math.random()*10);
+ console.log(num7);
+
+ //Integers between 0-100
+ var num8 = Math.round(Math.random()*100);
+ console.log(num8);
+
+ //Get a random integer between any 2 values
+ // Math.random() * (max-min) + min
+
+ //Random between 50-->80
+ var num9 = Math.round(Math.random()*(80-50)+50);
+  num9 = num9.toFixed(2);
+ console.log(num9);
+
+ 
