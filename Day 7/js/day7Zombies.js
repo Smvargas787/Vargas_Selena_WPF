6d2ff397e4379ce34eba02bdd983f00215f7d3ec
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-10 20:06:49 -0500

    End of zombies. For loop style

---
+++ Day 7/js/day7Zombies.js
@@ -0,0 +1,47 @@
+
+ /*
+Selena Vargas
+Zombies
+WPF 1411
+11/10/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+//Zombie Attack!!!
+ //We have a zombie attack at the school.
+ //It can bite 4 people a day turn them into zombies by the next day.
+ //The CDC has asked us, how many zombies will it
+
+
+
+ var numZombies = 1; //How many zombies do we have
+
+ var numBites = 4; //Number of bites, per zombie per day
+
+ var days = 100; //Number of days
+
+
+
+ //Every day zombies will bite 4 people and turn them into zombies
+
+ //Loop number of days
+
+ for(var i=1; i<=days; i++){
+
+  //How many new zombies get made and when do they go bite people.
+  var newZombies = numZombies*numBites;
+
+  //The bitten people now become zombies
+  numZombies = newZombies+numZombies; //numZombies +=newZombies;
+
+  console.log("There are now "+numZombies+" zombies on day #"+i+"!");
+
+  if(numZombies>=1000000){
+   break;
+
+
+
+  }
+
+ }
