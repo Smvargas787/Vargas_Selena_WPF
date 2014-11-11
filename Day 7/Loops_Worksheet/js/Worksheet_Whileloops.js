260503901357b711ac547d3fd476c26fd3cc4248
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-10 22:36:16 -0500

    While Loops Worksheet

---
+++ Day 7/Loops_Worksheet/js/Worksheet_Whileloops.js
@@ -0,0 +1,17 @@
+
+ /*
+Selena Vargas
+While Loops Worksheet
+WPF 1411
+11/10/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+//Five Little Monkeys Jumping on the bed Nursery rhyme
+
+ for(var i = 5; i>0; i--){
+
+
+ }
+
