ae28f6e78e955dfb8817856917c751d264d00440
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-10 19:47:03 -0500

    Finishing for loops
    Beer

--- Day 7/js/day7Beer.js
+++ Day 7/js/day7Beer.js
@@ -7,4 +7,31 @@ WPF 1411
 
 //alert("Testing 1, 2, 3!");
 
+// Console.log the whole song of 99 Bottles of Beer on the wall.
 
+ //Setup loop
+ //for(intialize variable; condition; increment of change){}
+
+ for(var i = 99; i>0; i--) {
+
+  //console.log(i);
+
+
+//Add an if statement testing for last case
+  if (i === 1) {
+   console.log(i + " bottle of beer on the wall. " + i + " bottle of beer. ");
+   console.log("You take one down, you pass it around. No more bottles of beer on the wall.");
+
+  } else if (i === 2) {
+   console.log(i + " bottles of beer on the wall. " + i + " bottle of beer. ");
+   console.log("You take one down, you pass it around. " + (i - 1) + " bottle of beer on the wall.");
+
+  }else {
+  console.log(i+" bottles of beer on the wall. "+i+" bottle of beer. ");
+  console.log("You take one down, you pass it around. "+(i-1)+" bottle of beer on the wall.");
+
+
+  }
+
+
+ }
