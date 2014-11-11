e9cd71a424022bac2853040f43f162d09090c2d6
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-10 22:47:15 -0500

    While Loops Worksheet

--- Day 7/Loops_Worksheet/js/Worksheet_Whileloops.js
+++ Day 7/Loops_Worksheet/js/Worksheet_Whileloops.js
@@ -10,8 +10,19 @@ WPF 1411
 
 //Five Little Monkeys Jumping on the bed Nursery rhyme
 
- for(var i = 5; i>0; i--){
+ for(var i = 5; i>1; i--){
 
+
+ }if(i===5){
+
+     console.log(+i+" little monkeys jumping on the bed."+i+" One fell off and bumped his head. Mama  called the doctor and the doctor said" +
+  " no more monkeys jumping on the bed.");
 
  }
+  else if(i===1){
+   console.log(+i+" little monkey jumping on the bed."+i+" fell off and bumped his head. Mama called the doctor and the doctor said" +
+   " put those monkeys right to bed.");
+
+
+  }
 
