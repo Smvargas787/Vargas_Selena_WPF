c885f59fa0c650abd52dfce448dba34c8b90bb7d
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-14 22:02:39 -0500

    Functions Personal (1)

---
+++ Day 9/js/Functions_Personal.js
@@ -0,0 +1,26 @@
+
+ /*
+Selena Vargas
+Functions Personal
+WPF 1411
+11/14/14
+ */
+
+//alert("Testing 1, 2, 3!");
+
+//You put a fraction of your weekly allowance in the bank for college.
+ //Find out how much you have in the bank currently
+ //and how much allowance you have earned.
+
+ var fraction = prompt("What fraction of your allowance did you put in your bank account?");
+
+ //Validation
+
+ while(fraction==="" || isNaN(fraction)){
+  //Reprompt
+  
+
+
+ }
+
+
