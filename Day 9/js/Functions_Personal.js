371d11cad7a618ca55ca22c99ebbc0e52cdb4de9
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-14 22:33:28 -0500

    Functions Personal (2)

--- Day 9/js/Functions_Personal.js
+++ Day 9/js/Functions_Personal.js
@@ -8,19 +8,36 @@ WPF 1411
 
 //alert("Testing 1, 2, 3!");
 
-//You put a fraction of your weekly allowance in the bank for college.
+//You put a percent of your weekly allowance in the bank for college.
  //Find out how much you have in the bank currently
  //and how much allowance you have earned.
 
- var fraction = prompt("What fraction of your allowance did you put in your bank account?");
+
+ var allowance = prompt("How much was your allowance?");
 
  //Validation
 
- while(fraction==="" || isNaN(fraction)){
-  //Reprompt
-  
+ while(allowance==="" || isNaN(allowance)){
+
+
+ }if(allowance===""){
+  allowance = prompt("Please don't leave blank!\nHow much do you currently have in your bank account?")
+
+
+ }else if(isNaN(allowance)){
+  allowance = prompt("Please only type in numbers!\nHow much do you currently have in your bank account?\nIe:10")
 
 
  }
 
 
+ var percent = prompt("What percent of your allowance did you put in your bank account?");
+
+ //Validation
+
+ while(percent==="" || isNaN(percent)){
+  //Reprompt
+  percent = prompt("Please don't leave blank and only use numbers!\nWhat percent of your allowance did you put in the bank?\nIe:30 ")
+
+
+ }
