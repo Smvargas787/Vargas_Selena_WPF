4c1fb934c875c316466ad774fb977824e64b9c27
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-15 23:57:31 -0500

    Wacky Functions (7)

--- Day 9/js/Functions_Wacky.js
+++ Day 9/js/Functions_Wacky.js
@@ -1,11 +1,17 @@
 
  /*
 Selena Vargas
-Assignment
+Wacky Functions
 WPF 1411
-Date
+11/14/14
  */
 
-alert("Testing 1, 2, 3!");
+//alert("Testing 1, 2, 3!");
+
+//Homer & Bart Simpson are trying to create an indoor swimming pool inside their house while Marge & Lisa are out shopping for the day
+ //Homer needs to find out how much water he would need to fill up his house before they return.
+ //Find out the volume of Homer Simpson's house.
+
+var height = prompt("What is the height of Homer Simpson's house?");
 
 
