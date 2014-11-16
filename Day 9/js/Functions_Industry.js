ecc329b2b604d68da7cbed0f1f5ee2ecba204a77
Smvargas787 <RicanChica@RicanChicas-MacBook-Pro.local>
2014-11-16 01:53:55 -0500

    Industry (16)

--- Day 9/js/Functions_Industry.js
+++ Day 9/js/Functions_Industry.js
@@ -8,5 +8,9 @@ WPF 1411
 
 //alert("Testing 1, 2, 3!");
 
+//Square Space wants you to make them some new templates for their users to build new websites for themselves.
+ //For each template you make they're willing to pay you $1,500.
+ //Find how how much you'll make after all your hard work on coding their templates is done.
 
+ 
 
